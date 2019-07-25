53e5b3f527dfc583832582cf449bea4e9cbe5d59
Vikram Jayanthi <vikram@pundit.today>
2019-07-25 11:07:00 -0700

    Made just the one change to votes.js

--- src/votes.js
+++ src/votes.js
@@ -2,5 +2,7 @@ module.exports = {
   votesRollCall: "https://api.propublica.org/congress/v1/{congress-number}/{chamber}/sessions/{session-number}/votes/{roll-call-number}{response-format}",
   votesByType: "https://api.propublica.org/congress/v1/{congress-number}/{chamber}/votes/{vote-type}{response-format}",
   votesByDate: "https://api.propublica.org/congress/v1/{chamber}/votes/{year}/{month}{response-format}",
-  votesNominations: "https://api.propublica.org/congress/v1/{congress-number}/nominations{response-format}"
+  votesNominations: "https://api.propublica.org/congress/v1/{congress-number}/nominations{response-format}",
+  votesRecent : "https://api.propublica.org/congress/v1/{chamber}/votes/recent{response-format}"
+
 };
